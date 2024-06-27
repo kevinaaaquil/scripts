8992e5d76e228167f413df245d7088e826af93ce
Eshan Singh <eshansingh2409@gmail.com>
2024-06-27 21:30:35 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -16,7 +16,7 @@ sudo docker ps -a
 sudo usermod -aG docker $USER
 
 # Download and install Docker Compose
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Show message
